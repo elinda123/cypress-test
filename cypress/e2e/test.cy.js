8f16d92689a8edae616255055bf471a8fb966958
Elinda T. <elinda.tragel@gmail.com>
2024-10-01 08:58:05 +0300

    Update test.cy.js

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,12 +1,22 @@
 describe('regionaalhaigla otsing', () => {
     
-    it('visit regionaalhaigla page', () => {
+    it('külasta regionaalhaigla lehte, kasuta otsingusõna ja ava artikkel', () => {
         cy.visit('https://www.regionaalhaigla.ee/')
         cy.get('#search-block-form')
-            .find('[type="text"]').type('teenetemargid')
+          .find('[type="text"]').type('teenetemärgid', { 
+              delay: 100
+          })
 
         cy.get('#search-block-form').submit()
-        cy.url().should('eq', 'https://www.regionaalhaigla.ee/et/search/site/teenetemargid')
+        cy.url({ decode: true }).should('contain', 'teenetemärgid')
+        cy.get('.search-result').should('contain', 'Presidendi teenetemärgi pälvisid dr Ülo Zirel ja dr Valdo Toome')
+            //.then(console.log)
+            .eq(6)
+            .scrollIntoView()
+            .find('a')
+            .click()
 
+        cy.url().should('contain', 'teenetemargi-palvisid-dr-ulo-zirel-ja-dr-valdo-toome')
+        cy.get('.date').should('contain', '07.02.2024')
     })
 })
